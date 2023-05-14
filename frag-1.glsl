2214d9c3709b464c48275e92ddfc3a03d11e1c40
Torstein Grindvik <torstein.grindvik@nordicsemi.no>
2023-05-14 13:57:38 +0200

    Two are ok
    Signed-off-by: Torstein Grindvik <torstein.grindvik@nordicsemi.no>

---
+++ frag-1.glsl
@@ -0,0 +1,24 @@
+precision mediump float;
+
+uniform float u_time;
+uniform vec2 u_resolution;
+
+#define S(a,b,x)smoothstep(a,b,x)
+
+void main()
+{
+	vec2 uv=gl_FragCoord.xy/u_resolution;
+	
+	float EPS=.01;
+	float W=2.*EPS;
+	
+	float ll=.5+.4*sin(u_time*.7);
+	
+	float lr=ll+W;
+	
+	float r=S(ll-EPS,ll+EPS,uv.y);
+	
+	r=r*S(lr+EPS,lr-EPS,uv.y);
+	
+	gl_FragColor=vec4(r,r,0.,1.);
+}
